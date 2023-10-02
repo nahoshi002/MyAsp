031a78660a0c77c82c06763d24bcda08ef9823f8
nahoshi002 <anhhaonguyen002@gmail.com>
2023-10-02 10:09:29 +0700

    Update UI 3th (Lap trinh web)

---
+++ SV20T1080031/SV20T1080031.Web/Areas/Admin/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SV20T1080031.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class EmployeeController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Thêm nhân viên mới
+        public IActionResult Create()
+        {
+            ViewBag.Title = "Bổ sung nhân viên";
+            return View();
+        }
+
+        // Chỉnh sửa thông tin nhân viên
+        public IActionResult Edit(int id = 0)
+        {
+            ViewBag.Title = "Cập nhật thông tin nhân viên";
+            return View("Create");
+        }
+
+        //Đổi mật khẩu
+        public IActionResult ChangePass(int id = 0)
+        {
+            return View();
+        }
+
+        //Xóa khách hàng
+        public IActionResult Delete(int id = 0)
+        {
+            return View();
+        }
+    }
+}
